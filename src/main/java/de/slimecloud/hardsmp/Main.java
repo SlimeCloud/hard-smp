c400006c5cec95de867cabdebbb23f252f64704f
cyklon73 <cyklon698@gmail.com>
2023-09-28 12:09:45 +0200

    Added methods to register Events and commands

--- src/main/java/de/slimecloud/hardsmp/Main.java
+++ src/main/java/de/slimecloud/hardsmp/Main.java
@@ -1,5 +1,8 @@
 package de.slimecloud.hardsmp;
 
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
@@ -13,5 +16,15 @@ public final class Main extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private void registerEvent(Listener listener) {
+        getServer().getPluginManager().registerEvents(listener, this);
+    }
+
+    private PluginCommand registerCommand(String name, CommandExecutor executor) {
+        PluginCommand command = getCommand(name);
+        if (command!=null) command.setExecutor(executor);
+        return command;
     }
 }

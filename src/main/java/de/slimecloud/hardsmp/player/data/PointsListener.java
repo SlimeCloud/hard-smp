85dea61b3e41c78b4a1ee673278f0239d8d11046
cyklon73 <cyklon698@gmail.com>
2023-10-24 22:20:36 +0200

    implemented CustomAdvancement points

--- src/main/java/de/slimecloud/hardsmp/player/data/PointsListener.java
+++ src/main/java/de/slimecloud/hardsmp/player/data/PointsListener.java
@@ -1,8 +1,11 @@
 package de.slimecloud.hardsmp.player.data;
 
 import de.cyklon.spigotutils.advancement.AdvancementType;
+import de.cyklon.spigotutils.advancement.DefaultAdvancement;
+import de.slimecloud.hardsmp.advancement.CustomAdvancement;
 import de.slimecloud.hardsmp.player.PlayerController;
 import io.papermc.paper.event.player.PlayerTradeEvent;
+import org.bukkit.NamespacedKey;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.Raid;
 import org.bukkit.attribute.Attribute;
@@ -31,85 +34,116 @@ public class PointsListener implements Listener {
     private final static Map<AdvancementType, Integer> ADVANCEMENTS_RARITY = new HashMap<>();
 
     static {
-        ADVANCEMENTS_RARITY.put(AdvancementType.DIAMONDS, 5);
-        ADVANCEMENTS_RARITY.put(AdvancementType.VOLUNTARY_EXILE, 5);
-        ADVANCEMENTS_RARITY.put(AdvancementType.INTO_FIRE, 5);
-        ADVANCEMENTS_RARITY.put(AdvancementType.HIDDEN_IN_THE_DEPTHS, 5);
-        ADVANCEMENTS_RARITY.put(AdvancementType.COUNTRY_LODE_TAKE_ME_HOME, 5);
-        ADVANCEMENTS_RARITY.put(AdvancementType.COVER_ME_WITH_DIAMONDS, 5);
-        ADVANCEMENTS_RARITY.put(AdvancementType.SPOOKY_SCARY_SKELETON, 5);
-        ADVANCEMENTS_RARITY.put(AdvancementType.FREE_THE_END, 5);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.DIAMONDS, 5);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.VOLUNTARY_EXILE, 5);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.INTO_FIRE, 5);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.HIDDEN_IN_THE_DEPTHS, 5);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.COUNTRY_LODE_TAKE_ME_HOME, 5);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.COVER_ME_WITH_DIAMONDS, 5);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.SPOOKY_SCARY_SKELETON, 5);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.FREE_THE_END, 5);
 
-        ADVANCEMENTS_RARITY.put(AdvancementType.STONE_AGE, 4);
-        ADVANCEMENTS_RARITY.put(AdvancementType.ENCHANTER, 4);
-        ADVANCEMENTS_RARITY.put(AdvancementType.EYE_SPY, 4);
-        ADVANCEMENTS_RARITY.put(AdvancementType.THE_END, 4);
-        ADVANCEMENTS_RARITY.put(AdvancementType.CRAFTING_A_NEW_LOOK, 4);
-        ADVANCEMENTS_RARITY.put(AdvancementType.GETTING_AN_UPGRADE, 4);
-        ADVANCEMENTS_RARITY.put(AdvancementType.GLOW_AND_BEHOLD, 4);
-        ADVANCEMENTS_RARITY.put(AdvancementType.THE_CITY_AT_THE_END_OF_THE_GAME, 4);
-        ADVANCEMENTS_RARITY.put(AdvancementType.WHAT_A_DEAL, 4);
-        ADVANCEMENTS_RARITY.put(AdvancementType.WE_NEED_TO_GO_DEEPER, 4);
-        ADVANCEMENTS_RARITY.put(AdvancementType.ISNT_IT_IRON_PICK, 4);
-        ADVANCEMENTS_RARITY.put(AdvancementType.WITHERING_HEIGHTS, 4);
-        ADVANCEMENTS_RARITY.put(AdvancementType.MONSTER_HUNTER, 4);
-        ADVANCEMENTS_RARITY.put(AdvancementType.FISHY_BUSINESS, 4);
-        ADVANCEMENTS_RARITY.put(AdvancementType.STICKY_SITUATION, 4);
-        ADVANCEMENTS_RARITY.put(AdvancementType.ACQUIRE_HARDWARE, 4);
-        ADVANCEMENTS_RARITY.put(AdvancementType.A_TERRIBLE_FORTRESS, 4);
-        ADVANCEMENTS_RARITY.put(AdvancementType.SUIT_UP, 4);
-        ADVANCEMENTS_RARITY.put(AdvancementType.NOT_TODAY_THANK_YOU, 4);
-        ADVANCEMENTS_RARITY.put(AdvancementType.CAREFUL_RESTORATION, 4);
-        ADVANCEMENTS_RARITY.put(AdvancementType.NOT_QUITE_NINE_LIVES, 4);
-        ADVANCEMENTS_RARITY.put(AdvancementType.TAKE_AIM, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.STONE_AGE, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.ENCHANTER, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.EYE_SPY, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.THE_END, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.CRAFTING_A_NEW_LOOK, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.GETTING_AN_UPGRADE, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.GLOW_AND_BEHOLD, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.THE_CITY_AT_THE_END_OF_THE_GAME, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.WHAT_A_DEAL, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.WE_NEED_TO_GO_DEEPER, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.ISNT_IT_IRON_PICK, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.WITHERING_HEIGHTS, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.MONSTER_HUNTER, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.FISHY_BUSINESS, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.STICKY_SITUATION, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.ACQUIRE_HARDWARE, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.A_TERRIBLE_FORTRESS, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.SUIT_UP, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.NOT_TODAY_THANK_YOU, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.CAREFUL_RESTORATION, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.NOT_QUITE_NINE_LIVES, 4);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.TAKE_AIM, 4);
 
-        ADVANCEMENTS_RARITY.put(AdvancementType.BRING_HOME_THE_BEACON, 3);
-        ADVANCEMENTS_RARITY.put(AdvancementType.A_THROWAWAY_JOKE, 3);
-        ADVANCEMENTS_RARITY.put(AdvancementType.A_SEEDY_PLACE, 3);
-        ADVANCEMENTS_RARITY.put(AdvancementType.LOCAL_BREWERY, 3);
-        ADVANCEMENTS_RARITY.put(AdvancementType.THOSE_WERE_THE_DAYS, 3);
-        ADVANCEMENTS_RARITY.put(AdvancementType.BUKKIT_BUKKIT, 3);
-        ADVANCEMENTS_RARITY.put(AdvancementType.WHOS_THE_PILLAGER_NOW, 3);
-        ADVANCEMENTS_RARITY.put(AdvancementType.HOT_STUFF, 3);
-        ADVANCEMENTS_RARITY.put(AdvancementType.THIS_BOAT_HAS_LEGS, 3);
-        ADVANCEMENTS_RARITY.put(AdvancementType.BEST_FRIENDS_FOREVER, 3);
-        ADVANCEMENTS_RARITY.put(AdvancementType.OL_BETSY, 3);
-        ADVANCEMENTS_RARITY.put(AdvancementType.TACTICAL_FISHING, 3);
-        ADVANCEMENTS_RARITY.put(AdvancementType.REMOTE_GETAWAY, 3);
-        ADVANCEMENTS_RARITY.put(AdvancementType.WHEN_THE_SQUAD_HOPS_INTO_TOWN, 3);
-        ADVANCEMENTS_RARITY.put(AdvancementType.OH_SHINY, 3);
-        ADVANCEMENTS_RARITY.put(AdvancementType.ICE_BUCKET_CHALLENGE, 3);
-        ADVANCEMENTS_RARITY.put(AdvancementType.SWEET_DREAMS, 3);
-        ADVANCEMENTS_RARITY.put(AdvancementType.PLANTING_THE_PAST, 3);
-        ADVANCEMENTS_RARITY.put(AdvancementType.THE_PARROTS_AND_THE_BATS, 3);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.BRING_HOME_THE_BEACON, 3);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.A_THROWAWAY_JOKE, 3);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.A_SEEDY_PLACE, 3);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.LOCAL_BREWERY, 3);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.THOSE_WERE_THE_DAYS, 3);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.BUKKIT_BUKKIT, 3);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.WHOS_THE_PILLAGER_NOW, 3);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.HOT_STUFF, 3);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.THIS_BOAT_HAS_LEGS, 3);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.BEST_FRIENDS_FOREVER, 3);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.OL_BETSY, 3);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.TACTICAL_FISHING, 3);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.REMOTE_GETAWAY, 3);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.WHEN_THE_SQUAD_HOPS_INTO_TOWN, 3);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.OH_SHINY, 3);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.ICE_BUCKET_CHALLENGE, 3);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.SWEET_DREAMS, 3);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.PLANTING_THE_PAST, 3);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.THE_PARROTS_AND_THE_BATS, 3);
 
-        ADVANCEMENTS_RARITY.put(AdvancementType.VERY_VERY_FRIGHTENING, 2);
-        ADVANCEMENTS_RARITY.put(AdvancementType.YOUVE_GOT_A_FRIEND_IN_ME, 2);
-        ADVANCEMENTS_RARITY.put(AdvancementType.TOTAL_BEELOCATION, 2);
-        ADVANCEMENTS_RARITY.put(AdvancementType.FEELS_LIKE_HOME, 2);
-        ADVANCEMENTS_RARITY.put(AdvancementType.CAVES_AND_CLIFFS, 2);
-        ADVANCEMENTS_RARITY.put(AdvancementType.WHATEVER_FLOATS_YOUR_GOAT, 2);
-        ADVANCEMENTS_RARITY.put(AdvancementType.WHO_IS_CUTTING_ONIONS, 2);
-        ADVANCEMENTS_RARITY.put(AdvancementType.SURGE_PROTECTOR, 2);
-        ADVANCEMENTS_RARITY.put(AdvancementType.WAR_PIGS, 2);
-        ADVANCEMENTS_RARITY.put(AdvancementType.STAR_TRADER, 2);
-        ADVANCEMENTS_RARITY.put(AdvancementType.SMELLS_INTERESTING, 2);
-        ADVANCEMENTS_RARITY.put(AdvancementType.THE_HEALING_POWER_OF_FRIENDSHIP, 2);
-        ADVANCEMENTS_RARITY.put(AdvancementType.RESPECTING_THE_REMNANTS, 2);
-        ADVANCEMENTS_RARITY.put(AdvancementType.BEE_OUR_GUEST, 2);
-        ADVANCEMENTS_RARITY.put(AdvancementType.THE_CUTEST_PREDATOR, 2);
-        ADVANCEMENTS_RARITY.put(AdvancementType.LIGHT_AS_A_RABBIT, 2);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.VERY_VERY_FRIGHTENING, 2);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.YOUVE_GOT_A_FRIEND_IN_ME, 2);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.TOTAL_BEELOCATION, 2);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.FEELS_LIKE_HOME, 2);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.CAVES_AND_CLIFFS, 2);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.WHATEVER_FLOATS_YOUR_GOAT, 2);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.WHO_IS_CUTTING_ONIONS, 2);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.SURGE_PROTECTOR, 2);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.WAR_PIGS, 2);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.STAR_TRADER, 2);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.SMELLS_INTERESTING, 2);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.THE_HEALING_POWER_OF_FRIENDSHIP, 2);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.RESPECTING_THE_REMNANTS, 2);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.BEE_OUR_GUEST, 2);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.THE_CUTEST_PREDATOR, 2);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.LIGHT_AS_A_RABBIT, 2);
 
-        ADVANCEMENTS_RARITY.put(AdvancementType.SNEAK_100, 1);
-        ADVANCEMENTS_RARITY.put(AdvancementType.SOUND_OF_MUSIC, 1);
-        ADVANCEMENTS_RARITY.put(AdvancementType.IS_IT_A_BIRD, 1);
-        ADVANCEMENTS_RARITY.put(AdvancementType.IS_IT_A_PLANE, 1);
-        ADVANCEMENTS_RARITY.put(AdvancementType.THE_POWER_OF_BOOKS, 1);
-        ADVANCEMENTS_RARITY.put(AdvancementType.BIRTHDAY_SONG, 1);
-        ADVANCEMENTS_RARITY.put(AdvancementType.LITTLE_SNIFFS, 1);
-        ADVANCEMENTS_RARITY.put(AdvancementType.IS_IT_A_BALLOON, 1);
-        ADVANCEMENTS_RARITY.put(AdvancementType.WAX_ON, 1);
-        ADVANCEMENTS_RARITY.put(AdvancementType.WAX_OFF, 1);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.SNEAK_100, 1);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.SOUND_OF_MUSIC, 1);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.IS_IT_A_BIRD, 1);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.IS_IT_A_PLANE, 1);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.THE_POWER_OF_BOOKS, 1);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.BIRTHDAY_SONG, 1);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.LITTLE_SNIFFS, 1);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.IS_IT_A_BALLOON, 1);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.WAX_ON, 1);
+        ADVANCEMENTS_RARITY.put(DefaultAdvancement.WAX_OFF, 1);
+
+
+
+
+
+
+
+        ADVANCEMENTS_RARITY.put(CustomAdvancement.GOLD3, 5);
+        ADVANCEMENTS_RARITY.put(CustomAdvancement.FROG, 5);
+        ADVANCEMENTS_RARITY.put(CustomAdvancement.BOAT, 5);
+        ADVANCEMENTS_RARITY.put(CustomAdvancement.LIGHTNING, 5);
+        ADVANCEMENTS_RARITY.put(CustomAdvancement.MUSIC, 5);
+        ADVANCEMENTS_RARITY.put(CustomAdvancement.POTTERY, 5);
+
+        ADVANCEMENTS_RARITY.put(CustomAdvancement.GOLD2, 4);
+        ADVANCEMENTS_RARITY.put(CustomAdvancement.BLOCKS, 4);
+        ADVANCEMENTS_RARITY.put(CustomAdvancement.GARDEN, 4);
+
+        ADVANCEMENTS_RARITY.put(CustomAdvancement.LUMBERJACK, 3);
+        ADVANCEMENTS_RARITY.put(CustomAdvancement.GOLD1, 3);
+        ADVANCEMENTS_RARITY.put(CustomAdvancement.GHAST, 3);
+        ADVANCEMENTS_RARITY.put(CustomAdvancement.DIAMOND, 3);
+        ADVANCEMENTS_RARITY.put(CustomAdvancement.ROOT, 3);
+
+        ADVANCEMENTS_RARITY.put(CustomAdvancement.TREASURE, 2);
+        ADVANCEMENTS_RARITY.put(CustomAdvancement.FISH, 2);
+        ADVANCEMENTS_RARITY.put(CustomAdvancement.FIREWORK, 2);
+
+        ADVANCEMENTS_RARITY.put(CustomAdvancement.LAVA, 1);
+        ADVANCEMENTS_RARITY.put(CustomAdvancement.CAVE, 1);
+        ADVANCEMENTS_RARITY.put(CustomAdvancement.BED, 1);
     }
 
     @EventHandler

5f6e4d56cc1ddbc65705d86929922a72c67e5395
cyklon73 <cyklon698@gmail.com>
2023-10-25 21:27:35 +0200

    implemented Advancements

--- src/main/java/de/slimecloud/hardsmp/advancement/handler/GhastAdvancement.java
+++ src/main/java/de/slimecloud/hardsmp/advancement/handler/GhastAdvancement.java
@@ -2,6 +2,9 @@ package de.slimecloud.hardsmp.advancement.handler;
 
 import de.slimecloud.hardsmp.advancement.AdvancementHandler;
 import de.slimecloud.hardsmp.advancement.CustomAdvancement;
+import org.bukkit.block.Biome;
+import org.bukkit.entity.Ghast;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.plugin.Plugin;
@@ -13,6 +16,13 @@ public class GhastAdvancement extends AdvancementHandler {
 
     @EventHandler
     public void onEntityKill(EntityDeathEvent event) {
+        if (event.getEntity() instanceof Ghast ghast) {
+            Player killer = ghast.getKiller();
+            if (killer!=null) {
+                if (killer.getLocation().getBlock().getBiome().equals(Biome.NETHER_WASTES)) {
 
+                }
+            }
+        }
     }
 }

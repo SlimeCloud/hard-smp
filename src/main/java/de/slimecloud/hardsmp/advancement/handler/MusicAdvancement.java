96ea92b1e92849b433ec1ec69392331d8f685c98
cyklon73 <cyklon698@gmail.com>
2023-11-05 12:26:35 +0100

    performance improvements

--- src/main/java/de/slimecloud/hardsmp/advancement/handler/MusicAdvancement.java
+++ src/main/java/de/slimecloud/hardsmp/advancement/handler/MusicAdvancement.java
@@ -1,21 +1,16 @@
 package de.slimecloud.hardsmp.advancement.handler;
 
-import de.cyklon.spigotutils.persistence.PersistentDataHandler;
 import de.slimecloud.hardsmp.advancement.AdvancementHandler;
 import de.slimecloud.hardsmp.advancement.CustomAdvancement;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
+import org.bukkit.Statistic;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerStatisticIncrementEvent;
 import org.bukkit.plugin.Plugin;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class MusicAdvancement extends AdvancementHandler {
 
@@ -26,28 +21,22 @@ public class MusicAdvancement extends AdvancementHandler {
 		for (Material value : Material.values()) if (value.name().contains("MUSIC_DISC")) DISCS.add(value);
 	}
 
-	private final NamespacedKey key;
-
 	public MusicAdvancement(Plugin plugin) {
 		super(plugin, CustomAdvancement.MUSIC);
-		this.key = new NamespacedKey(plugin, "discs.played");
 	}
 
 	@EventHandler
-	public void onInteract(PlayerInteractEvent event) {
-		if (!event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) return;
-		if (event.getClickedBlock() == null || !event.getClickedBlock().getType().equals(Material.JUKEBOX) || event.getItem() == null) return;
+	public void onStatisticIncrement(PlayerStatisticIncrementEvent event) {
+		if (event.getStatistic().equals(Statistic.USE_ITEM)) {
+			if (!DISCS.contains(event.getMaterial())) return;
+			Player player = event.getPlayer();
+			if (isDone(player)) return;
+			if (checkDiscs(player)) unlock(player);
+		}
+	}
 
-		Material material = event.getItem().getType();
-		int index = DISCS.indexOf(material);
-		if (index == -1) return;
-		Player player = event.getPlayer();
-		if (isDone(player)) return;
-		Set<Integer> collected = Arrays.stream(PersistentDataHandler.get(player).reviseIntArrayWithDefault(key, a -> {
-			Set<Integer> set = Arrays.stream(a).boxed().collect(Collectors.toSet());
-			set.add(index);
-			return set.stream().mapToInt(i -> i).toArray();
-		}, new int[0])).boxed().collect(Collectors.toSet());
-		if (collected.size() == DISCS.size()) unlock(player);
+	private boolean checkDiscs(Player player) {
+		for (Material disc : DISCS) if (player.getStatistic(Statistic.USE_ITEM, disc)==0) return false;
+		return true;
 	}
 }

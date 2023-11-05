3b6a1337b81cafcdec7c5945c83de59fe0f3bf62
cyklon73 <cyklon698@gmail.com>
2023-11-05 12:06:45 +0100

    optimized imports

--- src/main/java/de/slimecloud/hardsmp/advancement/handler/BedAdvancement.java
+++ src/main/java/de/slimecloud/hardsmp/advancement/handler/BedAdvancement.java
@@ -1,15 +1,9 @@
 package de.slimecloud.hardsmp.advancement.handler;
 
-import de.cyklon.spigotutils.persistence.PersistentDataHandler;
 import de.slimecloud.hardsmp.advancement.AdvancementHandler;
 import de.slimecloud.hardsmp.advancement.CustomAdvancement;
 import org.bukkit.Bukkit;
-import org.bukkit.NamespacedKey;
 import org.bukkit.Statistic;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerBedEnterEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
 
 public class BedAdvancement extends AdvancementHandler {

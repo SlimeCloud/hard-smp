dca18be1a49548892c994287da85afb670c251b9
Matito <matitos@outlook.de>
2023-11-08 22:51:06 +0100

    added TeamChat Command

---
+++ src/main/java/de/slimecloud/hardsmp/commands/TeamChatCommand.java
@@ -0,0 +1,39 @@
+package de.slimecloud.hardsmp.commands;
+
+import de.cyklon.spigotutils.adventure.Formatter;
+import de.slimecloud.hardsmp.HardSMP;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+
+public class TeamChatCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+
+        if (!commandSender.hasPermission(command.getPermission())) commandSender.sendMessage(command.permissionMessage());
+
+        for (Player player : HardSMP.getInstance().getServer().getOnlinePlayers()) {
+            if (!player.hasPermission("hardsmp.teamchat.read")) continue;
+
+            String format = HardSMP.getInstance().getConfig().getString("teamchat.messageFormat");
+            String messageColor = HardSMP.getInstance().getConfig().getString("teamchat.messageColor");
+
+            Component messageFormat = Formatter.parseText(format.replace("%player", commandSender.getName()));
+
+            StringBuilder message = new StringBuilder();
+            Arrays.stream(args).toList().forEach(arg -> message.append(" ").append(arg));
+
+            player.sendMessage(messageFormat.append(Formatter.parseText(messageColor + message)));
+
+        }
+
+        return true;
+    }
+}

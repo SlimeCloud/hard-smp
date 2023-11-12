8762e40f58ee16bd43270519d36f5a3138cda820
Matito <matitos@outlook.de>
2023-11-12 19:48:28 +0100

    implements EmptyTabCompleter + code reformat

--- src/main/java/de/slimecloud/hardsmp/commands/TeamChatCommand.java
+++ src/main/java/de/slimecloud/hardsmp/commands/TeamChatCommand.java
@@ -3,7 +3,6 @@ package de.slimecloud.hardsmp.commands;
 import de.cyklon.spigotutils.adventure.Formatter;
 import de.slimecloud.hardsmp.HardSMP;
 import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,12 +11,13 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 
-public class TeamChatCommand implements CommandExecutor {
+public class TeamChatCommand implements CommandExecutor, EmptyTabCompleter {
 
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
 
-        if (!commandSender.hasPermission(command.getPermission())) commandSender.sendMessage(command.permissionMessage());
+        if (!commandSender.hasPermission(command.getPermission()))
+            commandSender.sendMessage(command.permissionMessage());
 
         for (Player player : HardSMP.getInstance().getServer().getOnlinePlayers()) {
             if (!player.hasPermission("hardsmp.teamchat.read")) continue;

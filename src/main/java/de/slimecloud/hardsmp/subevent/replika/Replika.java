ee06d5a7db60f87230d1ac2a7584184789b2e34e
cyklon73 <cyklon698@gmail.com>
2023-11-14 18:52:31 +0100

    added plot.yml

--- src/main/java/de/slimecloud/hardsmp/subevent/replika/Replika.java
+++ src/main/java/de/slimecloud/hardsmp/subevent/replika/Replika.java
@@ -1,26 +1,42 @@
 package de.slimecloud.hardsmp.subevent.replika;
 
 import de.slimecloud.hardsmp.build.Build;
+import de.slimecloud.hardsmp.subevent.SubEvent;
 import lombok.Getter;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-public class Replika {
+@Getter
+public class Replika implements SubEvent {
 
-    @Getter
     private final File directory;
-    @Getter
+    private final int plotWidth;
+    private final int plotLength;
+    private final int plotSpacing;
+    private final Build plotSchematic;
     private final Set<Build> schematics;
+
+    private final List<Player> players;
 
 
     public Replika(Plugin plugin) {
         this.directory = new File(plugin.getDataFolder(), "replika/schematics");
+        YamlConfiguration plotConfig = YamlConfiguration.loadConfiguration(new File(plugin.getDataFolder(), "plot.yml"));
+        this.plotWidth = plotConfig.getInt("plot.width");
+        this.plotLength = plotConfig.getInt("plot.length");
+        this.plotSpacing = plotConfig.getInt("plot.spacing");
+        try {
+            this.plotSchematic = Build.load(new File(plugin.getDataFolder(), "replika/" + plotConfig.getString("plot.schematic")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         this.schematics = new HashSet<>();
+        this.players = new ArrayList<>();
         directory.mkdirs();
         if (directory.listFiles()!=null) {
             Arrays.stream(directory.listFiles())
@@ -44,4 +60,24 @@ public class Replika {
     }
 
 
+    @Override
+    public void start(Collection<Player> players) {
+        this.players.addAll(players);
+
+    }
+
+    @Override
+    public void join(Player player) {
+        this.players.add(player);
+    }
+
+    @Override
+    public void leave(Player player) {
+        this.players.remove(player);
+    }
+
+    @Override
+    public Collection<Player> getPlayers() {
+        return new ArrayList<>(players);
+    }
 }

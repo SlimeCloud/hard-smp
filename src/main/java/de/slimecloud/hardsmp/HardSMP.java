ac637fd30084ca5bcbf0e8b1778d5541ce8ee192
Matito <matitos@outlook.de>
2023-11-15 22:01:26 +0100

    added multible items to shop 2/3
    - Invisible item frame (still todo)
    - light Block
    - Mending Infinity Bow

--- src/main/java/de/slimecloud/hardsmp/HardSMP.java
+++ src/main/java/de/slimecloud/hardsmp/HardSMP.java
@@ -13,12 +13,9 @@ import de.slimecloud.hardsmp.ui.Chat;
 import de.slimecloud.hardsmp.ui.Tablist;
 import de.slimecloud.hardsmp.ui.scoreboard.ScoreboardManager;
 import de.slimecloud.hardsmp.verify.MinecraftVerificationListener;
-import lombok.ConfigurationKeys;
 import lombok.Getter;
-import lombok.SneakyThrows;
 import me.lucko.spark.api.Spark;
 import me.lucko.spark.api.SparkProvider;
-import net.dv8tion.jda.api.JDA;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
@@ -29,10 +26,11 @@ import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.PluginCommand;
-import org.bukkit.configuration.ConfigurationOptions;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemFlag;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HardSMP extends JavaPlugin {
@@ -86,6 +84,9 @@ public final class HardSMP extends JavaPlugin {
         registerCommand("teamchat", new TeamChatCommand());
 
         itemManager.registerItem("chest-key", () -> new ItemBuilder(Material.IRON_HOE).addItemFlags(ItemFlag.HIDE_ATTRIBUTES).setDisplayName(ChatColor.RESET + "Chest Key").build());
+        itemManager.registerItem("invisible-frame", () -> new ItemBuilder(Material.ITEM_FRAME).setDisplayName(ChatColor.GREEN + "Invisible Item Frame") .build()); //ToDo so it invisible
+        itemManager.registerItem("light-block", () -> new ItemBuilder(Material.LIGHT).build());
+        itemManager.registerItem("mending-Infinity-bow", () -> new ItemBuilder(Material.BOW).addEnchantment(Enchantment.ARROW_INFINITE, 1).addEnchantment(Enchantment.MENDING, 1) .build());
 
         SlimeHandler.setupOffers(getConfig());
 

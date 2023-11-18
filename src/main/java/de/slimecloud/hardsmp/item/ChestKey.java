813d120a5d8433e74c8c397e4150a5d43439ca46
Artus <artuslama@gmail.com>
2023-11-19 00:57:49 +0100

    Implemented secure chest-locking
    (not grief prove!)

--- src/main/java/de/slimecloud/hardsmp/item/ChestKey.java
+++ src/main/java/de/slimecloud/hardsmp/item/ChestKey.java
@@ -1,24 +1,24 @@
 package de.slimecloud.hardsmp.item;
 
 import com.google.common.primitives.Longs;
-import de.cyklon.spigotutils.adventure.Formatter;
 import de.cyklon.spigotutils.persistence.PersistentDataHandler;
-import net.kyori.adventure.text.Component;
+import de.cyklon.spigotutils.tuple.Tuple;
+import de.cyklon.spigotutils.adventure.Formatter;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
+import org.bukkit.block.Chest;
 import org.bukkit.block.Container;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemFlag;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.*;
 import org.bukkit.plugin.Plugin;
 
 import java.util.*;
@@ -37,8 +37,11 @@ public class ChestKey extends CustomItem implements Listener {
 
         this.plugin = plugin;
         this.LOCKABLE = new HashSet<>();
-        List<String> list = plugin.getConfig().getStringList("chest-key.lockable");
+        Set<String> list = new HashSet<>(plugin.getConfig().getStringList("chest-key.lockable"));
         list.forEach(s -> LOCKABLE.add(Material.valueOf(s.toUpperCase())));
+        if (LOCKABLE.contains(Material.SHULKER_BOX)) for (Material value : Material.values()) {
+            if (value.name().contains("SHULKER_BOX")) LOCKABLE.add(value);
+        }
         this.idKey = new NamespacedKey(plugin, "lock-id");
         add();
     }
@@ -56,13 +59,13 @@ public class ChestKey extends CustomItem implements Listener {
                     if (LOCKABLE.contains(clickedBlock.getType()) && clickedBlock.getState() instanceof Container container) {
                         if (isContainerLocked(container)) {
                             if (player.isSneaking()) {
-                                unbindKey(container, item);
+                                unbindKey(event.getClickedBlock(), item);
                                 event.getPlayer().sendActionBar(Formatter.parseText(plugin.getConfig().getString("chest-key.success.unlock", "§2Geöffnet")));
                                 flag = false;
                             }
                         } else {
                             if (player.isSneaking()) {
-                                bindKey(container, item);
+                                bindKey(event.getClickedBlock(), item);
                                 event.getPlayer().sendActionBar(Formatter.parseText(plugin.getConfig().getString("chest-key.success.lock", "§2Verschlossen")));
                                 flag = false;
                             }
@@ -74,7 +77,7 @@ public class ChestKey extends CustomItem implements Listener {
                     if (clickedBlock.getState() instanceof Container container && isContainerLocked(container)) {
                         if (!playerHasKeyForContainer(player, getID(container))) {
                             player.sendActionBar(Formatter.parseText(plugin.getConfig().getString("chest-key.no-key", "§cVerschlossen")));
-                            event.setCancelled(true);
+                            event.setCancelled(!event.getPlayer().isSneaking());
                         }
                     }
                 } else event.setCancelled(true);
@@ -82,6 +85,26 @@ public class ChestKey extends CustomItem implements Listener {
             } else if (isItem(event.getItem())) event.setCancelled(true);
         }
     }
+
+    @EventHandler
+    @SuppressWarnings("ConstantConditions")
+    public void onBlockPlace(BlockPlaceEvent event) {
+        if (!(event.getBlock().getState() instanceof Chest)) return;
+        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+            Tuple<Boolean, Inventory, Inventory> isDoubleChest = isDoubleChest(event.getBlockPlaced());
+            if (isDoubleChest.first()) {
+                Inventory left = isDoubleChest.second();
+                Inventory right = isDoubleChest.third();
+
+                boolean leftHasId = event.getBlockPlaced().getLocation().equals(right.getLocation());
+                if (isInventoryBlockLocked(leftHasId ? left : right)) {
+                    long id = getID((Container) (leftHasId ? left : right).getLocation().getBlock().getState());
+                    bindIdToBlock((leftHasId ? right : left).getLocation().getBlock(), id);
+                }
+            }
+        }, 1);
+    }
+
 
     @EventHandler
     public void onItemMoveInInventory(InventoryMoveItemEvent event) {
@@ -92,7 +115,7 @@ public class ChestKey extends CustomItem implements Listener {
         if (inventory instanceof PlayerInventory) return false;
         if (inventory.getLocation() != null) {
             Block block = inventory.getLocation().getBlock();
-            if (LOCKABLE.contains(block.getType())) return isContainerLocked(block.getState());
+            return isContainerLocked(block.getState());
         }
         return false;
     }
@@ -104,6 +127,19 @@ public class ChestKey extends CustomItem implements Listener {
      */
     private boolean isContainerLocked(BlockState block) {
         return LOCKABLE.contains(block.getType()) && block instanceof Container container && PersistentDataHandler.get(container).contains(idKey);
+    }
+
+    private Tuple<Boolean, Inventory, Inventory> isDoubleChest(Block block) {
+        boolean isDoubleChest = false;
+        Inventory leftSide = null;
+        Inventory rightSide = null;
+
+        if (block.getState() instanceof Chest chest && chest.getInventory() instanceof DoubleChestInventory doubleChest) {
+            isDoubleChest = true;
+            leftSide = doubleChest.getLeftSide();
+            rightSide = doubleChest.getRightSide();
+        }
+        return new Tuple<>(isDoubleChest, leftSide, rightSide);
     }
 
 
@@ -126,32 +162,67 @@ public class ChestKey extends CustomItem implements Listener {
         return PersistentDataHandler.get(key).getLongArray(idKey);
     }
 
+
+    private void bindIdToBlock(Block block, long id) {
+        if (block.getState() instanceof Container blockContainer) {
+            PersistentDataHandler.get(blockContainer).set(idKey, id);
+            blockContainer.update();
+        }
+    }
+
     /**
      * bind a container to a key
-     * @param container the container to be bound to the key
+     * @param block the container to be bound to the key
      * @param key the key to which the container is to be bound
      */
-    private void bindKey(Container container, ItemStack key) {
-        long id = System.nanoTime();
-        PersistentDataHandler data = PersistentDataHandler.get(container);
-        data.set(idKey, id);
-        container.update();
-        data = PersistentDataHandler.get(key);
-        Set<Long> ids = new HashSet<>(Longs.asList(Objects.requireNonNullElse(data.getLongArray(idKey), new long[0])));
-        ids.add(id);
-        data.set(idKey, Longs.toArray(ids));
+    private void bindKey(Block block, ItemStack key) {
+        if (block.getState() instanceof Container) {
+            Tuple<Boolean, Inventory, Inventory> isDoubleChest = isDoubleChest(block);
+            List<Block> lockBlocks = new ArrayList<>();
+            if (isDoubleChest.first() && isDoubleChest.second().getLocation() != null && isDoubleChest.third().getLocation() != null) {
+                lockBlocks.add(isDoubleChest.second().getLocation().getBlock());
+                lockBlocks.add(isDoubleChest.third().getLocation().getBlock());
+            } else lockBlocks.add(block);
+
+            long id = System.nanoTime();
+            for (Block lockBlock : lockBlocks) {
+                bindIdToBlock(lockBlock, id);
+            }
+
+            PersistentDataHandler data = PersistentDataHandler.get(key);
+            Set<Long> ids = new HashSet<>(Longs.asList(Objects.requireNonNullElse(data.getLongArray(idKey), new long[0])));
+            ids.add(id);
+            data.set(idKey, Longs.toArray(ids));
+        }
     }
 
     @SuppressWarnings("ConstantConditions")
-    private void unbindKey(Container container, ItemStack key) {
-        PersistentDataHandler data = PersistentDataHandler.get(container);
-        if (!data.contains(idKey)) return;
-        long id = data.getLong(idKey);
-        data.remove(idKey);
-        container.update();
-        Set<Long> ids = new HashSet<>(Longs.asList(Objects.requireNonNullElse(data.getLongArray(idKey), new long[0])));
-        ids.remove(id);
-        data.set(idKey, Longs.toArray(ids));
+    private void unbindKey(Block block, ItemStack key) {
+        if (block.getState() instanceof Container) {
+            Tuple<Boolean, Inventory, Inventory> isDoubleChest = isDoubleChest(block);
+            List<Block> lockBlocks = new ArrayList<>();
+            if (isDoubleChest.first() && isDoubleChest.second().getLocation() != null && isDoubleChest.third().getLocation() != null) {
+                lockBlocks.add(isDoubleChest.second().getLocation().getBlock());
+                lockBlocks.add(isDoubleChest.third().getLocation().getBlock());
+            } else lockBlocks.add(block);
+
+            PersistentDataHandler data;
+            long id = 0;
+            for (Block lockBlock : lockBlocks) {
+                if (lockBlock.getState() instanceof Container blockContainer) {
+                    data = PersistentDataHandler.get(blockContainer);
+                    if (!data.contains(idKey)) return;
+                    id = data.getLong(idKey);
+                    data.remove(idKey);
+                    blockContainer.update();
+                }
+            }
+
+            data = PersistentDataHandler.get(key);
+            Set<Long> ids = new HashSet<>(Longs.asList(Objects.requireNonNullElse(data.getLongArray(idKey), new long[0])));
+            ids.remove(id);
+            data.set(idKey, Longs.toArray(ids));
+        }
     }
 
     private boolean playerHasKeyForContainer(Player player, long id) {

8be49053e56576aa1351c661ff35cd6ee162cb51
MineKing <mineking9534@gmail.com>
2023-11-27 19:14:37 +0100

    Fix MinecraftInfoCommand

--- src/main/java/de/slimecloud/hardsmp/info/MinecraftInfoCommand.java
+++ src/main/java/de/slimecloud/hardsmp/info/MinecraftInfoCommand.java
@@ -1,7 +1,5 @@
 package de.slimecloud.hardsmp.info;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
 import de.slimecloud.hardsmp.HardSMP;
 import de.slimecloud.hardsmp.player.PlayerController;
 import de.slimecloud.hardsmp.verify.Verification;
@@ -12,48 +10,33 @@ import net.dv8tion.jda.api.entities.User;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
-import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.time.Duration;
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 public class MinecraftInfoCommand implements CommandExecutor, TabCompleter {
-	private final Cache<String, List<String>> cache = CacheBuilder.newBuilder()
-			.expireAfterWrite(Duration.ofMinutes(10))
-			.build();
-
 	@Override
 	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 		if(args.length != 2) return false;
 
 		JDA jda = HardSMP.getInstance().getDiscordBot().jdaInstance;
 
-		User user = null;
-		OfflinePlayer player = null;
+		User user;
+		OfflinePlayer player;
 
 		switch(args[0]) {
 			case "discord" -> {
 				try {
-					user = jda.getUserById(args[1]);
-				} catch(NumberFormatException ignored) {
-				}
-
-				try {
-					if(user == null) user = jda.getGuildById(HardSMP.getInstance().getConfig().getLong("discord.guild")).getMembersByEffectiveName(args[0], true).get(1).getUser();
+					user = jda.getGuildById(HardSMP.getInstance().getConfig().getLong("discord.guild")).getMembersByEffectiveName(args[1], true).get(0).getUser();
 				} catch(NullPointerException | IndexOutOfBoundsException ignored) {
-				}
-
-				if(user == null) {
 					sender.sendMessage(Component.text("Nutzer nicht gefunden!").color(NamedTextColor.RED));
 					return true;
 				}
@@ -65,16 +48,11 @@ public class MinecraftInfoCommand implements CommandExecutor, TabCompleter {
 					return true;
 				}
 
-				player = Bukkit.getOfflinePlayer(verification.getMinecraftID());
+				player = Bukkit.getOfflinePlayer(UUID.fromString(verification.getMinecraftID()));
 			}
 
 			case "minecraft" -> {
-				try {
-					player = Bukkit.getOfflinePlayer(UUID.fromString(args[1]));
-				} catch(IllegalArgumentException ignored) {
-				}
-
-				if(player == null) player = Bukkit.getOfflinePlayer(args[1]);
+				player = Bukkit.getOfflinePlayer(args[1]);
 
 				Verification verification = Verification.load(player.getUniqueId().toString());
 
@@ -96,8 +74,9 @@ public class MinecraftInfoCommand implements CommandExecutor, TabCompleter {
 			return true;
 		}
 
-		sender.sendMessage(Component.text("Informationen zu ").color(TextColor.color(0x88D657)).append(Component.text(user.getEffectiveName()).color(TextColor.color(0xF6ED82))).appendNewline()
+		sender.sendMessage(Component.text("---- Informationen zu ").color(TextColor.color(0x88D657)).append(Component.text(user.getEffectiveName()).color(TextColor.color(0xF6ED82)).decorate(TextDecoration.BOLD)).decoration(TextDecoration.BOLD, false).append(Component.text(" ----")).appendNewline()
 				.append(Component.text("Minecraft Name: ").color(TextColor.color(0x88D657)).append(Component.text(player.getName()).color(TextColor.color(0xF6ED82)))).appendNewline()
+				.append(Component.text("Minecraft UUID: ").color(TextColor.color(0x88D657)).append(Component.text(player.getUniqueId().toString()).color(TextColor.color(0xF6ED82)))).appendNewline()
 				.append(Component.text("Discord Name: ").color(TextColor.color(0x88D657)).append(Component.text(user.getEffectiveName()).color(TextColor.color(0xF6ED82)))).appendNewline()
 				.append(Component.text("Punkte: ").color(TextColor.color(0x88D657)).append(Component.text((int) PlayerController.getPlayer(player).getActualPoints()).color(TextColor.color(0xF6ED82)))).appendNewline()
 		);
@@ -117,17 +96,17 @@ public class MinecraftInfoCommand implements CommandExecutor, TabCompleter {
 		else if(args.length == 2) {
 			switch(args[0]) {
 				case "discord" -> {
-					return cache.get(args[0], () -> HardSMP.getInstance().getDiscordBot().jdaInstance.getGuildById(HardSMP.getInstance().getConfig().getLong("discord.guild")).getMembers().stream()
+					return HardSMP.getInstance().getDiscordBot().jdaInstance.getGuildById(HardSMP.getInstance().getConfig().getLong("discord.guild")).getMembers().stream()
 							.map(Member::getEffectiveName)
-							.filter(u -> u.startsWith(args[1]))
-							.toList()
-					);
+							.filter(u -> u.contains(args[1]))
+							.toList();
 				}
 
 				case "minecraft" -> {
-					return Bukkit.getOnlinePlayers().stream()
-							.map(Player::getName)
-							.filter(p -> p.startsWith(args[1]))
+					return Arrays.stream(Bukkit.getOfflinePlayers())
+							.map(OfflinePlayer::getName)
+							.filter(Objects::nonNull)
+							.filter(p -> p.contains(args[1]))
 							.toList();
 				}
 			}

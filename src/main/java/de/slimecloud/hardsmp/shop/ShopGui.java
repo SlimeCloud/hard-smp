346207ecbff440ab05ed08d8e1911619b8108d96
cyklon73 <cyklon698@gmail.com>
2023-11-29 20:50:04 +0100

    fixed required points display

--- src/main/java/de/slimecloud/hardsmp/shop/ShopGui.java
+++ src/main/java/de/slimecloud/hardsmp/shop/ShopGui.java
@@ -25,7 +25,7 @@ public class ShopGui {
         int i = 0;
         for (Offer offer : offers) {
             Price price = offer.price();
-            MerchantRecipe recipe = new MerchantRecipe(itemManager.getBuilder(offer.item()).addLore(List.of("", ChatColor.LIGHT_PURPLE + "benötige punkte: " + price.requiredPoints())).setLocalizedName(String.valueOf(i++)).setAmount(offer.amount()).build(), Integer.MAX_VALUE);
+            MerchantRecipe recipe = new MerchantRecipe(itemManager.getBuilder(offer.item()).addLore(List.of("", ChatColor.LIGHT_PURPLE + "benötige punkte: " + Math.round(price.requiredPoints()))).setLocalizedName(String.valueOf(i++)).setAmount(offer.amount()).build(), Integer.MAX_VALUE);
             Material mat;
             if ((mat = Material.getMaterial(price.firstItem().toUpperCase())) == null) {
                 HardSMP.getInstance().getLogger().warning("skipped offer '" + offer.item() + "' cause item '" + price.firstItem().toUpperCase() + "' not found");

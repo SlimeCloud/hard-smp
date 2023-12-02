fae6ebc9e6acb0697de61058896677b82c732433
Matito <matitos@outlook.de>
2023-12-02 23:11:39 +0100

    fix and improve

--- src/main/java/de/slimecloud/hardsmp/commands/HatItemCommand.java
+++ src/main/java/de/slimecloud/hardsmp/commands/HatItemCommand.java
@@ -2,29 +2,45 @@ package de.slimecloud.hardsmp.commands;
 
 import de.cyklon.spigotutils.adventure.Formatter;
 import de.slimecloud.hardsmp.HardSMP;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 public class HatItemCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 
-        if (sender instanceof Player player) {
-            Player targetPlayer = player;
-            if (args.length > 0) {
-                Player tempPlayer = Bukkit.getPlayer(args[0]);
-                if (tempPlayer != null) targetPlayer = tempPlayer;
-                else {
-                    sender.sendMessage(Formatter.parseText(HardSMP.getInstance().getConfig().getString("hatitem.playerNotFoundErrorMessage", "§cSpieler wurde nicht gefunden!")));
-                    return true;
-                }
+        if (!(sender instanceof Player)) sender.sendMessage("Der Command kann nur als Spieler ausgeführt werden!");
+        Player sendPlayer = Bukkit.getPlayer(sender.getName());
+        Player targetPlayer = sendPlayer;
+        if (args.length > 0) {
+            targetPlayer = Bukkit.getPlayer(args[0]);
+            if (targetPlayer == null) {
+                sender.sendMessage(Formatter.parseText(HardSMP.getInstance().getConfig().getString("hatitem.playerNotFoundErrorMessage", "§cSpieler wurde nicht gefunden!")));
+                return true;
             }
-            targetPlayer.getInventory().setHelmet(player.getInventory().getItemInMainHand());
-        } else sender.sendMessage("Der Command kann nur als Spieler ausgeführt werden!");
+        }
+        if (!(targetPlayer.getInventory().getItem(EquipmentSlot.HEAD).getType() == Material.AIR)) {
+            sendPlayer.sendMessage(HardSMP.getPrefix().append(Component.text("§cDer Helm slot ist bereits belegt", NamedTextColor.RED)));
+            return true;
+        }
+        ItemStack item = sendPlayer.getInventory().getItemInMainHand();
+        if (item.getType() == Material.AIR) {
+            sendPlayer.sendMessage(HardSMP.getPrefix().append(Component.text("Nimm ein Material in die Hand welches du auf den Kopf setzen willst!", NamedTextColor.RED)));
+            return true;
+        }
+        targetPlayer.getInventory().setHelmet(item);
+        sendPlayer.getInventory().remove(item);
+        targetPlayer.sendMessage(HardSMP.getPrefix().append(Component.text("Dir wurde " + item.getType().toString().toLowerCase().replace("_", " ") + " auf den Kopf gesetzt!", TextColor.color(0x88d657))));
 
         return true;
     }

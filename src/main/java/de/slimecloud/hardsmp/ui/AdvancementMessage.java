1e240bb2a4c394094a1331bc7696f544f9cb28b4
MineKing <mineking9534@gmail.com>
2023-12-03 02:04:57 +0100

    Fix custom advancements

--- src/main/java/de/slimecloud/hardsmp/ui/AdvancementMessage.java
+++ src/main/java/de/slimecloud/hardsmp/ui/AdvancementMessage.java
@@ -1,5 +1,8 @@
 package de.slimecloud.hardsmp.ui;
 
+import de.cyklon.spigotutils.advancement.AdvancementType;
+import de.slimecloud.hardsmp.player.data.PointCategory;
+import de.slimecloud.hardsmp.player.data.PointsListener;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.event.EventHandler;
@@ -11,10 +14,17 @@ public class AdvancementMessage implements Listener {
     private void onAdvancement(PlayerAdvancementDoneEvent event) {
         if(event.getAdvancement().getDisplay() == null) return;
 
+        AdvancementType type = AdvancementType.getAdvancementType(event.getAdvancement());
+        Integer rarity = PointsListener.ADVANCEMENTS_RARITY.get(type.getKey());
+        if(rarity == null) return;
+
         event.message(Chat.getName(event.getPlayer())
                 .append(Component.text(" hat das Advancement ").color(NamedTextColor.GRAY))
                 .append(event.getAdvancement().getDisplay().displayName())
-                .append(Component.text(" bekommen").color(NamedTextColor.GRAY))
+                .append(Component.text(" bekommen ").color(NamedTextColor.GRAY))
+                .append(Component.text("[").color(NamedTextColor.DARK_GRAY))
+                .append(Component.text(PointCategory.ADVANCEMENT.calculate(rarity)).color(NamedTextColor.RED))
+                .append(Component.text("]").color(NamedTextColor.DARK_GRAY))
         );
     }
 }

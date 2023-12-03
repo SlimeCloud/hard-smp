60ba809a550e0f490b9677c82e7d081f008784b8
MineKing <mineking9534@gmail.com>
2023-12-03 10:08:19 +0100

    Fix point display in death message

--- src/main/java/de/slimecloud/hardsmp/ui/JoinMessage.java
+++ src/main/java/de/slimecloud/hardsmp/ui/JoinMessage.java
@@ -1,10 +1,11 @@
 package de.slimecloud.hardsmp.ui;
 
-import de.slimecloud.hardsmp.HardSMP;
+import de.slimecloud.hardsmp.player.EventPlayer;
+import de.slimecloud.hardsmp.player.PlayerController;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.Statistic;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -24,10 +25,13 @@ public class JoinMessage implements Listener {
 
     @EventHandler
     public void onDeath(PlayerDeathEvent event) {
+        EventPlayer player = PlayerController.getPlayer(Bukkit.getOfflinePlayer(event.getPlayer().getUniqueId()));
+        double points = Math.min(event.getPlayer().getStatistic(Statistic.DEATHS) * 100, player.getPoints());
+
         event.deathMessage(Chat.getName(event.getPlayer())
                 .append(Component.text(" ist gestorben ").color(NamedTextColor.GRAY))
                 .append(Component.text("[").color(NamedTextColor.DARK_GRAY))
-                .append(Component.text("-" + event.getPlayer().getStatistic(Statistic.DEATHS) * 100).color(NamedTextColor.RED))
+                .append(Component.text("-" + (int) points).color(NamedTextColor.RED))
                 .append(Component.text("]").color(NamedTextColor.DARK_GRAY))
         );
     }

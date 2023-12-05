44550e541622006c215a9662565a9cb14174ebf5
MineKing <mineking9534@gmail.com>
2023-12-05 21:08:56 +0100

    Remove warning

--- src/main/java/de/slimecloud/hardsmp/claim/ClaimProtectionHandler.java
+++ src/main/java/de/slimecloud/hardsmp/claim/ClaimProtectionHandler.java
@@ -21,14 +21,7 @@ import java.util.List;
 public class ClaimProtectionHandler implements Listener {
 
     private boolean isClaimed(Location loc, Player player) {
-        if (Claim.allClaims.values().stream().anyMatch(claim -> claim.contains(loc) && !claim.getUuid().equals(player.getUniqueId().toString()))) {
-            if (player.hasPermission("hardsmp.claim.bypass")) {
-                player.sendMessage(Component.text("Du führst eine Aktion in geclaimtem Gebiet aus!", NamedTextColor.GOLD));
-                return false;
-            }
-            return true;
-        }
-        return false;
+        return Claim.allClaims.values().stream().anyMatch(claim -> claim.contains(loc) && !claim.getUuid().equals(player.getUniqueId().toString()));
     }
 
     @EventHandler

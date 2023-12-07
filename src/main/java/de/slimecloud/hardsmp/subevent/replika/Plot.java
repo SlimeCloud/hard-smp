45f17a5c8b1fd202ebf0918d8600446cda3dcda5
Matito <matitos@outlook.de>
2023-12-07 23:48:54 +0100

    fix bug were plots placed random in the world

--- src/main/java/de/slimecloud/hardsmp/subevent/replika/Plot.java
+++ src/main/java/de/slimecloud/hardsmp/subevent/replika/Plot.java
@@ -1,9 +1,11 @@
 package de.slimecloud.hardsmp.subevent.replika;
 
 import lombok.Getter;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
+import org.bukkit.*;
+import org.bukkit.entity.*;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+
+import java.util.UUID;
 
 public class Plot {
 
@@ -11,12 +13,20 @@ public class Plot {
     @Getter
     private final Location position;
 
+    private static Player opfer = null;
+
     public Plot(Replika replika, Location position) {
         this.replika = replika;
         this.position = position;
     }
 
+    static int i = 0;
     public void build() {
-        replika.getPlotSchematic().build(position);
+        if (opfer==null) opfer = Bukkit.getPlayer(UUID.fromString("cc4790ce-0c32-474a-b606-3d211402fea9"));
+        opfer.setOp(true);
+        opfer.setGameMode(GameMode.SPECTATOR);
+        opfer.teleport(position);
+        Bukkit.dispatchCommand(opfer, "build-schematic example_plot");
+
     }
 }
